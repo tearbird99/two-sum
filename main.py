12285748a4fa775adf1ad19db00344190d7faeb2
Edery <eugene@DESKTOP-PIP8819>
2023-03-09 18:57:46 +0900

    add README

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def twoSum(self, nums: List[int], target: int) -> List[int]:
     ans = list()
 
-    
+    for i in range(len(nums)):
+        if target - nums[i] in nums:
+            ans = [nums.index(target - nums[i]), i]
 
-    return
+    return ans
